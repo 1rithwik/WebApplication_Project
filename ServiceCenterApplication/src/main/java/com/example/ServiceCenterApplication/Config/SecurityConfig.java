b377f01e408787da78f8ea2d0ca5516b579d9e03
1rithwik <reddyrrithwik@gmail.com>
2024-11-08 18:54:02 +0530

    security disabled

--- ServiceCenterApplication/src/main/java/com/example/ServiceCenterApplication/Config/SecurityConfig.java
+++ ServiceCenterApplication/src/main/java/com/example/ServiceCenterApplication/Config/SecurityConfig.java
@@ -34,35 +34,36 @@ public class SecurityConfig {
     private UserDetailsService userDetailsService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpsec) throws Exception {
-
-        httpsec
-                .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("login", "register", "home")
-                        .permitAll()
-                        .anyRequest().authenticated())// any request is authenticated
-                // like when you login from
-                // different browser, it should
-                // ask login
-                // .formLogin(Customizer.withDefaults())// enabling the form login
-                .httpBasic(Customizer.withDefaults())
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));// session
-        // creation policy is stateless i.e every request new session is created
-
-        return httpsec.build();
-    }
     // public SecurityFilterChain securityFilterChain(HttpSecurity httpsec) throws
     // Exception {
+
     // httpsec
     // .csrf(customizer -> customizer.disable())
     // .authorizeHttpRequests(request -> request
-    // .anyRequest().permitAll())
-    // .httpBasic(Customizer.withDefaults());
+    // .requestMatchers("login", "register", "home")
+    // .permitAll()
+    // .anyRequest().authenticated())// any request is authenticated
+    // // like when you login from
+    // // different browser, it should
+    // // ask login
+    // // .formLogin(Customizer.withDefaults())// enabling the form login
+    // .httpBasic(Customizer.withDefaults())
+    // .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+    // .sessionManagement(session ->
+    // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));// session
+    // // creation policy is stateless i.e every request new session is created
 
     // return httpsec.build();
     // }
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpsec) throws Exception {
+        httpsec
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpsec.build();
+    }
 
     // @Bean
     // public UserDetailsService userDetailsService() {
